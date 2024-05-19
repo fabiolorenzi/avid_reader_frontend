d5d2332f09a1146d86f8919d4d030aaee8625d12
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-05-19 09:14:39 +0100

    developed basic navigation bar component

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import {GiBookshelf} from "react-icons/gi";
+import {FaRegHeart} from "react-icons/fa";
+import {GoGear} from "react-icons/go";
+import "./NavigationBar.css";
+
+function NavigationBar() {
+
+    // The following function is to get the right className for
+    // active and not active NavLink
+    function getRightClass(target: string): string {
+        const pathname: string = window.location.pathname;
+        return target.includes(pathname) ? "navigationBar_active" : "navigationBar_notActive";
+    };
+
+    return(
+        <div className="navigationBar_container">
+            <div>
+                <NavLink
+                    to="/dashboard"
+                    className={getRightClass("/dashboard")}
+                >
+                    <GiBookshelf />
+                    <div className="space_line"></div>
+                </NavLink>
+                <NavLink
+                    to="/dashboard"
+                    className={getRightClass("/favorites")}
+                >
+                    <FaRegHeart />
+                    <div className="space_line"></div>
+                </NavLink>
+                <NavLink
+                    to="/dashboard"
+                    className={getRightClass("/settings")}
+                >
+                    <GoGear />
+                    <div className="space_line"></div>
+                </NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default NavigationBar;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import NavigationBar from "../../components/NavigationBar";
+import {Helmet} from "react-helmet";
+import Header from "../../components/Header";
 import "./Dashboard.css";
 
 function Dashboard() {
     return(
-        <div className="dashboard_container">Hello world</div>
+        <div className="dashboard_container">
+            <Helmet>
+                <title>Avid Reader | Dashboard</title>
+            </Helmet>
+            <Header />
+            <div className="dashboard_main">
+                <NavigationBar />
+                <div className="dashboard_page"></div>
+            </div>
+        </div>
     );
 };
 
